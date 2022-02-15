912bc4e7649b53907be0b8f702dc2c40145a9d77
Emmanuel Hernández <emmanuel_general@hotmail.com>
2022-02-14 20:15:33 -0600

    Add files via upload

---
+++ Kata del Módulo 10/config.py
@@ -0,0 +1,7 @@
+def main():
+    try:
+        configuration = open('config.txt')
+    except FileNotFoundError:
+        print("Couldn't find the config.txt file!")
+    except IsADirectoryError:
+        print("Found config.txt but it is a directory, couldn't read it")
